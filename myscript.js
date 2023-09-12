51a6d4a79725f830500084dc6970a6e114665dd1
DEVILRAIDER7 <55130246+DEVILRAIDER7@users.noreply.github.com>
2023-09-12 08:50:55 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,33 @@
-myscript
+// Get the DOM elements
+const taskInput = document.querySelector("#task");
+const addBtn = document.querySelector("#add");
+const todosDiv = document.querySelector("#todos");
+
+// Add event listener to the "Add Task" button
+addBtn.addEventListener("click", () => {
+    // Get the task from the input field
+    const task = taskInput.value;
+
+    // Create a new list item for the task
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    // Create a "Remove" button for the task
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    removeBtn.classList.add("remove");
+    removeBtn.addEventListener("click", () => {
+        // Remove the task from the list when the "Remove" button is clicked
+        li.remove();
+    });
+
+    // Append the "Remove" button to the list item
+    li.appendChild(removeBtn);
+
+    // Append the list item to the "todos" div
+    todosDiv.appendChild(li);
+
+    // Clear the input field
+    taskInput.value = "";
+});
+
